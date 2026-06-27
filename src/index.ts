48dc8337c521bcbbfe3c5d06d81d480246fc7755
connorakey <connorakey@proton.me>
2026-06-27 10:20:33 +1000

    feat: add bot login capabilities

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import { Client, Events, GatewayIntentBits } from "discord.js";
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+});
+
+client.once(Events.ClientReady, () => {
+    console.log(`Logged in as ${client.user?.tag}!`);
+});
+    
+
+client.login(process.env.DISCORD_BOT_TOKEN);
